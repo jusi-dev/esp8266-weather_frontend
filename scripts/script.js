35176749fceac32016c0a4cd11e1892f5c747afa
Justin Winistoerfer <winistoerfer.justin@gmx.ch>
2023-05-29 21:53:10 +0200

    Added new featues

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,12 @@ let barHTML = document.getElementById("bar");
 let gasHTML = document.getElementById("gas");
 let lastUpdateHTML = document.getElementById("last");
 
+let tempHTML2 = document.getElementById("temp2");
+let humidityHTML2 = document.getElementById("hum2");
+let barHTML2 = document.getElementById("bar2");
+let gasHTML2 = document.getElementById("gas2");
+let lastUpdateHTML2 = document.getElementById("last2");
+
 window.addEventListener("load", (event) => {
     console.log("Site is ready");
     getWeatherData();
@@ -19,20 +25,18 @@ async function getWeatherData () {
     fetch("https://weather-backend-3hcx.onrender.com/api/weatherLoc1")
       .then(response => response.json())
       .then((data) => {
-        let weatherData = data
-        const date = new Date();
-        const timeHour = date.getHours();
-        const timeMinutes = date.getMinutes();
+        weatherData = data
         gotData = true
         temp = weatherData["temp"]
         humidity = weatherData["humidity"]
         bar = weatherData["bar"]
-        gas =weatherData["gas"]
+        gas = weatherData["gas"]
+        updated = weatherData["updated"]
         tempHTML.innerHTML = `${temp} Grad`;
         humidityHTML.innerHTML = `${humidity}%`;
         barHTML.innerHTML = `${bar} Pa`;
         gasHTML.innerHTML = `${gas}%`;
-        lastUpdateHTML.innerHTML = `${timeHour}:${timeMinutes}`;
+        lastUpdateHTML.innerHTML = `${updated}`;
       })
       .catch((err) => {
         console.log(err)
@@ -40,5 +44,41 @@ async function getWeatherData () {
         humidity = "NULL"
         bar = "NULL"
         gas = "NULL"
+        updated = "NULL"
+        tempHTML.innerHTML = `${temp} Grad`;
+        humidityHTML.innerHTML = `${humidity}%`;
+        barHTML.innerHTML = `${bar} Pa`;
+        gasHTML.innerHTML = `${gas}%`;
+        lastUpdateHTML.innerHTML = `${updated}`;
+      })
+
+    fetch("https://weather-backend-3hcx.onrender.com/api/weatherLoc2")
+      .then(response => response.json())
+      .then((data) => {
+        weatherData2 = data
+        gotData = true
+        temp2 = weatherData2["temp"]
+        humidity2 = weatherData2["humidity"]
+        bar2 = weatherData2["bar"]
+        gas2 = weatherData2["gas"]
+        updated2 = weatherData2["updated"]
+        tempHTML2.innerHTML = `${temp} Grad`;
+        humidityHTML2.innerHTML = `${humidity}%`;
+        barHTML2.innerHTML = `${bar} Pa`;
+        gasHTML2.innerHTML = `${gas}%`;
+        lastUpdateHTML2.innerHTML = `${updated}`;
+      })
+      .catch((err) => {
+        console.log(err)
+        temp2 = "NULL"
+        humidity2 = "NULL"
+        bar2 = "NULL"
+        gas2 = "NULL"
+        updated2 = "NULL"
+        tempHTML2.innerHTML = `${temp} Grad`;
+        humidityHTML2.innerHTML = `${humidity}%`;
+        barHTML2.innerHTML = `${bar} Pa`;
+        gasHTML2.innerHTML = `${gas}%`;
+        lastUpdateHTML2.innerHTML = `${updated}`;
       })
 }
